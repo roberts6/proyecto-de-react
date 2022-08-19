eb2d4d327162ba1493e67f581895471ad10c2062
roberts6 <roberts_oscar@hotmail.com>
2022-08-19 18:23:51 -0500

    se agrega imagen a la card

--- src/App.js
+++ src/App.js
@@ -20,9 +20,11 @@ function App() {
         />
       <div className = "cards">
         <ZapatillasCard
-        image = "../src/img/airJordan.svg"
+        image = "https://cdn-images.farfetch-contents.com/17/68/92/00/17689200_37154678_1000.jpg"
+        //image = "./components/img/airJordan.svg"
         name = "Air Jordan 1 Mid - Negro" 
         price = "$1200"
+        description = "Logo Air Jordan Wings característico, detalle del logo, panel en contraste, detalle de perforaciones, puntera redonda, cierre con agujetas en la parte delantera, parche del logo en la lengüeta, diseño por el tobillo, plantilla con logo y suela de goma. Material: Cuero. Estos estilos son suministrados por un marketplace de tenis, el cual ofrece los artículos más codiciados y difíciles de encontrar de todo el mundo.."
         />
       </div>  
       </body>
--- src/components/CardComponent/ZapatillasCard.js
+++ src/components/CardComponent/ZapatillasCard.js
@@ -7,19 +7,25 @@ import { CardActionArea } from '@mui/material';
 //import img from "../../img/airJordan.svg"
 
 
-export default function ActionAreaCard({name, price, image}) {
+export default function ZapatillasCard({name, price, description, image}) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
-          
+        component="img"
+        height="240"
+        image={image}
+        alt="Air Jordan 1 Mid"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {name}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography variant="body3" color="text.secondary">
             {price}
+          </Typography>
+          <Typography variant="body1" size="text.secondary">
+            {description}
           </Typography>
         </CardContent>
       </CardActionArea>
