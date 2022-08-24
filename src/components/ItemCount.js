bc64832a8bfe3cb1ca337d5a89b8c125594bda2d
roberts6 <roberts_oscar@hotmail.com>
2022-08-24 18:07:20 -0500

    se hace contador usando stock,initial

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,31 +1,64 @@
 import React, {useState} from "react"
 
+// const ItemCount = () => {
+//     const [counter, setCounter] = useState(0)
+//     const addButton = () => {
+//         if(counter < 6){
+//             setCounter(counter + 1)
+//         }else {
+//             alert("Se acabó el stock")
+//         }
+//     }
+//     const substractButton = () => {
+//         if (counter >= 1) {
+//             setCounter(counter - 1)
+//         }
+//         else{
+//             alert("Tu carrito está vacío")
+//         }
+//     }
+//     return ( 
+//     <div>
+//         <p >
+//             {counter}
+//         </p>
+//         <button onClick={addButton}>+</button>
+//         <button onClick={substractButton}>-</button>
+//     </div>
+//     )
+// }
+
 const ItemCount = () => {
-    const [counter, setCounter] = useState(0)
-    const addButton = () => {
-        if(counter < 6){
-            setCounter(counter + 1)
-        }else {
-            alert("Se acabó el stock")
-        }
+    const stock = 10
+    const [initial, setInitial] = useState(0);
+    const onAdd = () => {
+        // if (initial < stock  ) {
+        //     setInitial(initial + 1);
+        // }
+        // else { return; }
+        const adding = (initial < stock) ? setInitial(initial + 1) : stock
+        console.log(adding)
     }
-    const substractButton = () => {
-        if (counter >= 1) {
-            setCounter(counter - 1)
-        }
-        else{
-            alert("Tu carrito está vacío")
-        }
+    const onSubstract = () => {
+        // if (initial > 0  ) {
+        //     setInitial(initial - 1);
+        // }
+        // else { return; }
+        const substracting = (initial > 0) ? setInitial(initial - 1) : stock
+        console.log(substracting)
     }
     return ( 
-    <div>
-        <p >
-            {counter}
-        </p>
-        <button onClick={addButton}>+</button>
-        <button onClick={substractButton}>-</button>
-    </div>
-    )
+            <div className= "handlerCart">
+                <p className="state">
+                    {initial}
+                </p>
+              <div className="handlerButtons">  
+                <button onClick={onAdd}>+</button>
+                <button onClick={onSubstract}>-</button>
+              </div>  
+                <button className= "botonAnadir">Añadir al carrito</button>
+            </div>
+            )
 }
 
 export default ItemCount;
