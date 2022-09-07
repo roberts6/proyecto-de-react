6b89b174219fcd475301756b6f1e0921057cccaf
roberts6 <roberts_oscar@hotmail.com>
2022-09-07 18:49:29 -0500

    no puedo hacer funcionar el map en itemList

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,12 +1,25 @@
 
-import React from "react"
+import React, {useEffect, useState} from 'react'
+import ItemList from "./ItemList"
 
 const ItemListContainer = ({greeting}) => {
+    const [Items, setItems] = useState([]);
+
+	useEffect(() => {    
+    setTimeout(() => {
+	  fetch('https://fakestoreapi.com/products')
+	  .then(response => response.json())
+      .then(data => setItems(data))
+      .catch(err => console.error(err));  
+    }, 2000);
+        },[]);
+
     return ( 
     <div className= "seraEste">
-        <p className = "greeting">
+        {/* <p className = "greeting">
             {greeting}
-        </p>
+        </p> */}
+        < ItemList data={Items}/>
     </div>
     )
 }
