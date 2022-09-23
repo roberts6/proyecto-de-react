a1d1006f0cc27b8c76f2604326009b99c0ec2e37
roberts6 <roberts_oscar@hotmail.com>
2022-09-22 21:34:55 -0500

    no consigo insertar la funcion totalPriceCart

--- src/components/cart.js
+++ src/components/cart.js
@@ -7,32 +7,37 @@ const Cart = () => {
   const {RemoveItem, cartList, totalPriceCart} = useItemContext()
   //console.log("esta es mi cartList en el cart",cartList.length);
   
+  
   if (cartList.length === 0) {
     return(
-    <div className = "cart-empty">
+      <div className = "cart-empty">
       <h1>No hay nada en tu carrito :(</h1>
       <Link to= "/">Vamos a comprar!!</Link>
     </div>
     )
-  } 
+  } else{
     return (
       cartList.map ((product) => { 
         return(
-          <div key={product.id} className = "cards-individual-fetch">
-      <div className= "cart">
-           <img src={product.image} alt= {product.title}/>
-        <div className= "cart-description">
-        <p>Producto: ${product.title}</p>
-           <p>Precio unitario: ${product.price}</p>
-           <p>Cantidad seleccionada: {product.quantity}</p>
-           <p>precio total: ${product.price * product.quantity}</p>
-           <button onClick={() => RemoveItem(product.id)}>Eliminar</button>
-        </div>   
-      </div>     
-    </div>
-        ) 
-      } )
-      )
+      <div key={product.id} className = "cards-individual-fetch">
+        <div className= "cart">
+             <img src={product.image} alt= {product.title}/>
+          <div className= "cart-description">
+          <p>Producto: ${product.title}</p>
+             <p>Precio unitario: ${product.price}</p>
+             <p>Cantidad seleccionada: {product.quantity}</p>
+             <p>precio total: ${product.price * product.quantity}</p>
+             <button onClick={() => RemoveItem(product.id)}>Eliminar</button>
+          </div>   
+        </div>     
+      </div>
+          ) 
+        },
+        console.log("el totalPriceCart es = ", totalPriceCart())
+        )
+        
+        )
+      }
     }
   
 export default Cart
