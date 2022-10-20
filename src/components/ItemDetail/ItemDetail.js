cd542c43a8b1badd7ffd41c96c8d929bcc1967b3
roberts6 <roberts_oscar@hotmail.com>
2022-10-20 09:37:31 -0500

    cambios en el itemDetail para seleccionar talle,por el momento solo estetico

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,11 +1,7 @@
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import Box from '@mui/material/Box';
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
-import Divider from '@mui/material/Divider';
+import { CardActionArea, Box, Divider, InputLabel, MenuItem, FormControl, Select } from '@mui/material';
 import ItemCount from "../ItemCount"
 import React, { useState } from 'react';
 import {useItemContext} from "../../CartContext"
@@ -15,16 +11,13 @@ import Swal from 'sweetalert2'
 
 
 const ItemDetail = ({data}) => {
-  //console.log("esta es la data del itemDetail",data);
   
 
     const [ setPurchase] = useState(false)
     //console.log("esta es la compra",purchase);
     const {AddItem} = useItemContext()
 
-    const onCart = (quantity) => {
-      //alert(`Se agrego: ${quantity}  ${data.name}`);
-      
+    const onCart = (quantity) => {    
       Swal.fire({
         title: `Agregaste ${quantity} del modelo ${data.name}`,
         text: `Seguro que querés ${quantity} en tu carrito?`,
@@ -46,7 +39,22 @@ const ItemDetail = ({data}) => {
       })
     };
 
-    
+    // MUI
+    const [age, setAge] = React.useState('');
+  const [open, setOpen] = React.useState(false);
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
     
     return(
       // data ? 
@@ -66,14 +74,44 @@ const ItemDetail = ({data}) => {
         <Typography gutterBottom variant="body1">
           Elige tu talle
         </Typography>
-        <Stack direction="row" spacing={1}>
+        {/* <Stack direction="row" spacing={1}>
           <Chip label="X Small" />
           <Chip color="primary" label="Small" />
           <Chip label="Medium" />
           <Chip label="Large" />
           <Chip label="XLarge" />
           <Chip label="XXLarge" />
-        </Stack>
+        </Stack> */}
+       
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-controlled-open-select-label">Talle</InputLabel>
+      <div className= "opciones-talles">
+        <Select
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          value={age}
+          label="Talle"
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>US size</em>
+          </MenuItem>
+          <MenuItem value={8}>US 8</MenuItem>
+          <MenuItem value={8.5}>US 8.5</MenuItem>
+          <MenuItem value={9}>US 9</MenuItem>
+          <MenuItem value={9.5}>US 9.5</MenuItem>
+          <MenuItem value={10}>US 10</MenuItem>
+          <MenuItem value={10.5}>US 10.5</MenuItem>
+          <MenuItem value={11}>US 11</MenuItem>
+          <MenuItem value={11.5}>US 11.5</MenuItem>
+          <MenuItem value={12}>US 12</MenuItem>
+        </Select>
+      </div>
+      </FormControl>
+        
         <Box sx={{ mt: 3, ml: 1, mb: 1 }}>
       </Box>
           <ItemCount stock={data.stock} setPurchase={true} name={data.name} id={data.id} img={data.image} initial={0} onCart={onCart} />
